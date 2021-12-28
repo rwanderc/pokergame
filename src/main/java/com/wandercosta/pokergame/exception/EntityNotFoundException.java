2c06077d4eec010d6d21bdddf69319e748bfe756
Wander Costa <rwander@gmail.com>
2021-12-28 18:19:51 +0100

    Removed src folder from root

--- src/main/java/com/wandercosta/pokergame/exception/EntityNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.wandercosta.pokergame.exception;
-
-public class EntityNotFoundException extends IllegalArgumentException {
-
-    public EntityNotFoundException(final String message) {
-        super(message);
-    }
-}
